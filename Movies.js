b82fd2053767be2cbaca6f0a6053b5b4256dc0e8
1NT18IS106 <83121712+1NT18IS106@users.noreply.github.com>
2021-07-16 10:50:54 +0530

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -58,25 +58,25 @@
 
 switched to db movies
 > db.films.aggregate([ { $match: { referrable: "TRUE"}}, { $group: { _id: "$movie_name", Avg_IMDB_Rating: { $avg : "$imdb_rating"}}} ])
-{ "_id" : "mirzapur", "Avg_IMDB_Rating" : 9.2 }
-{ "_id" : "ghazi attack", "Avg_IMDB_Rating" : 6.4 }
-{ "_id" : "pathan", "Avg_IMDB_Rating" : 8.8 }
-{ "_id" : "raat akeli hai", "Avg_IMDB_Rating" : 7.8 }
-{ "_id" : "special ops", "Avg_IMDB_Rating" : 9.2 }
-{ "_id" : "toofan", "Avg_IMDB_Rating" : 8.6 }
-{ "_id" : "haseen dilruba", "Avg_IMDB_Rating" : 8.8 }
-{ "_id" : "sanju", "Avg_IMDB_Rating" : 9.2 }
-{ "_id" : "deewar", "Avg_IMDB_Rating" : 7.2 }
+{ "_id" : "V", "Avg_IMDB_Rating" : 9.3 }
+{ "_id" : "Red", "Avg_IMDB_Rating" : 6.1 }
+{ "_id" : "Jersey", "Avg_IMDB_Rating" : 8.8 }
+{ "_id" : "Alive", "Avg_IMDB_Rating" : 7.2 }
+{ "_id" : "Poison", "Avg_IMDB_Rating" : 9.1 }
+{ "_id" : "Lucifer", "Avg_IMDB_Rating" : 9.2 }
+{ "_id" : "Elite", "Avg_IMDB_Rating" : 8.8 }
+{ "_id" : "Dhoni", "Avg_IMDB_Rating" : 9.2 }
+{ "_id" : "sultan", "Avg_IMDB_Rating" : 7.2 }
 { "_id" : "family man", "Avg_IMDB_Rating" : 9.4 }
-{ "_id" : "uri", "Avg_IMDB_Rating" : 9.5 }
+{ "_id" : "Bahubali", "Avg_IMDB_Rating" : 9.5 }
 >
 >
 > db.films.aggregate([ { $match: { genre: "suspense"}}, { $group: { _id: "$movie_name", Avg_IMDB_Rating: { $avg : "$imdb_rating"}}}  ])
-{ "_id" : "special ops", "Avg_IMDB_Rating" : 9.2 }
-{ "_id" : "aarya", "Avg_IMDB_Rating" : 8.1 }
-{ "_id" : "tiger zinda hai", "Avg_IMDB_Rating" : 9.2 }
-{ "_id" : "haseen dilruba", "Avg_IMDB_Rating" : 8.8 }
-{ "_id" : "uri", "Avg_IMDB_Rating" : 9.5 }
+{ "_id" : "Family man", "Avg_IMDB_Rating" : 9.4 }
+{ "_id" : "Elite", "Avg_IMDB_Rating" : 8.2 }
+{ "_id" : "Bahubali", "Avg_IMDB_Rating" : 9.5 }
+{ "_id" : "Anna", "Avg_IMDB_Rating" : 8.8 }
+{ "_id" : "Alive", "Avg_IMDB_Rating" : 7.2 }
 >
 
 ____________________________________________________________________________________________________________________________________________________
@@ -91,12 +91,12 @@ ________________________________________________________________________________
 > db.films.mapReduce(mapper,reduce, {out : "Feedbackout"})
 { "result" : "Feedbackout", "ok" : 1 }
 > db.Feedbackout.find().pretty()
-{ "_id" : "comedy", "value" : 67892 }
-{ "_id" : "drama", "value" : 197103 }
-{ "_id" : "thriller", "value" : 142522 }
-{ "_id" : "suspense", "value" : 205623 }
-{ "_id" : "sports", "value" : 34789 }
-{ "_id" : "romantic", "value" : 114419 }
+{ "_id" : "comedy", "value" : 34567 }
+{ "_id" : "drama", "value" : 12456 }
+{ "_id" : "thriller", "value" : 24567 }
+{ "_id" : "suspense", "value" : 24786}
+{ "_id" : "sports", "value" : 35672 }
+{ "_id" : "romantic", "value" : 45678 }
 
 
 
@@ -107,7 +107,7 @@ ________________________________________________________________________________
 3. Count the number of Movies which belong to the thriller category and find out the total number of positive reviews in that category.
 
 > db.films.aggregate([{ $group : {_id : { genre : "thriller"}, "Total Positive Reviews" : {$sum : "$feedback"}}}])
-{ "_id" : { "genre" : "thriller" }, "Total Positive Reviews" : 762348 }
+{ "_id" : { "genre" : "thriller" }, "Total Positive Reviews" : 678733 }
 
 
 ____________________________________________________________________________________________________________________________________________________
@@ -116,9 +116,9 @@ ________________________________________________________________________________
 4. Group all the records by genre and find out the total number of positive feedbacks by genre.
 
  db.films.aggregate([{ $group : {_id : "$genre", "Total Positive Reviews" : {$sum : "$feedback"}}}]).pretty()
-{ "_id" : "thriller", "Total Positive Reviews" : 142522 }
-{ "_id" : "comedy", "Total Positive Reviews" : 67892 }
-{ "_id" : "drama", "Total Positive Reviews" : 197103 }
-{ "_id" : "romantic", "Total Positive Reviews" : 114419 }
-{ "_id" : "sports", "Total Positive Reviews" : 34789 }
-{ "_id" : "suspense", "Total Positive Reviews" : 205623 }
+{ "_id" : "thriller", "Total Positive Reviews" : 678733}
+{ "_id" : "comedy", "Total Positive Reviews" : 457682 }
+{ "_id" : "drama", "Total Positive Reviews" : 245787}
+{ "_id" : "romantic", "Total Positive Reviews" : 346754}
+{ "_id" : "sports", "Total Positive Reviews" : 334257 }
+{ "_id" : "suspense", "Total Positive Reviews" : 564327}
